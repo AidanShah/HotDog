006cc345104177fa328696e0b7c9da2bc8c6528b
Aidan Shah <aidan_shah@horacemann.org>
2023-04-18 09:52:05 -0400

    k

--- index.js
+++ index.js
@@ -1,61 +1,40 @@
-const path = require("path");
-const sqlite = require("sqlite3").verbose();
-const dbFile = path.join(__dirname, "hotdog.db");
-const db = new sqlite.Database(dbFile, (error) => {
-  if (error) return console.error(error.message);
-  console.log(`Connected to database ${dbFile}`);
+const express = require("express");
+const app = express();
+const db = require("./db");
+const PORT = 4000;
+
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:8080",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
 });
 
-const login = (request, response) => {
-    // Parse the id to generate a SQLite query
-    const username = request.params.username;
-    const password = request.params.password;
-    const query = `SELECT * FROM user WHERE username = ?`;
-    db.get(query, [username], (error, result) => {
-      if (error) {
-        console.error(error.message);
-        response.status(400).json({ error: error.message });
-        return;
-      }
-      // If nothing is returned, then result will be undefined
-      if (result) {
-        if(json(result)['password']= password){
-            passwordcheck = {"correct_password":true}
-            response.passwordcheck
-        }
-        else{
-            passwordcheck = {"correct_password":false}
-        }
-      } else {
-        response.sendStatus(404);
-      }
-    });
-  };
+app.get("/", (req, res) => {
+  res.json({ info: "Demo app for sqlite3" });
+});
 
+app.get("/login/:username/:password", db.login)
+app.get("/highscore/:username", db.getHighScore)
+app.put("/change/:username/:password/:newpassword", db.changePassword)
 
-  const changePassword = (request, response) => {
-    // Parse the id to generate a SQLite query
-    const username = request.params.username;
-    const password = request.params.password;
-    const newpassword = request.params.newpassword;
-    const query = `UPDATE user SET password = ? WHERE username = ? and password = ?`;
-    db.get(query, [newpassword, username, password], (error, result) => {
-      if (error) {
-        console.error(error.message);
-        response.status(400).json({ error: error.message });
-        return;
-      }
-      // If nothing is returned, then result will be undefined
-      if (result) {
-        passwordchange = {"successful_change":true}
-        response.passwordchange
-      } else {
-        response.sendStatus(404);
-      }
-    });
-  };
+// ------ FILL IN BELOW -------
+// Write endpoints that allow a client to:
 
-  module.exports = {
-    login,
-    changePassword,
-  };
+//-----------------------------
+//#region Server
+//-----------------------------
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+//#endregion Server

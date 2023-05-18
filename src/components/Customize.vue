832523b131122204f44b28d5bee101803671d51f
Carmen Zhang <jwzhang06@gmail.com>
2023-05-18 10:08:51 -0400

    generated hd

--- src/components/Customize.vue
+++ src/components/Customize.vue
@@ -5,15 +5,26 @@ import NavBar from './NavBar.vue'
 export default {
     components: { NavBar, },
     data() {
+        const imageLinkArray = [
+            {
+                imageLink: "https://www.pngmart.com/files/5/Donuts-PNG-File.png",
+            },
+            {
+                imageLink: "https://www.pngall.com/wp-content/uploads/11/Donut-No-Background.png",
+            }
+        ];
+
         return {
             src: "https://images.squarespace-cdn.com/content/v1/5dd8630d09ab5908e35b35a0/1574462664001-48S1VO61L9LNWNJ910CV/img-HotDogStock-1080x675.png?format=1000w",
-            imageLink: null,
             hotdog: "default", 
-            imageLink: ''
+            imageLink: '',
+
         };
     },
     methods: {
+        generateHotDog(){
 
+        },
 
     }
 }
@@ -43,6 +54,15 @@ export default {
             Put the url of the Image you want:
         </p>
         <input class="input-box" type="input-box" v-model="imageLink" placeholder="Enter text">
+
+        <p>
+            OR <br>
+            Generate a randomly generated hotdog
+        </p>
+
+        <button v-on:click="generateHotDog">
+            Generate
+        </button>
     
     </center>
 
